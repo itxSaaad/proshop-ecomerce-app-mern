85f7857c17c3e118da58ed8579ab727017e4e372
itxSaaad <saadstudent.cs@gmail.com>
2022-10-16 20:17:39 +0500

    Added: Custom Error Handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import { config } from "dotenv";
 import cors from "cors";
 import colors from "colors";
 
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 
@@ -11,11 +13,16 @@ connectDB();
 
 const app = express();
 
-app.use("/api/products", productRoutes);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
