6a67fe884b3c9c5c842ce5c800f9ea678cbe67c8
itxSaaad <saadstudent.cs@gmail.com>
2022-10-27 21:35:08 +0500

    Added: User Auth & Generate Token

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import colors from "colors";
-import bodyParser from "body-parser";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 config();
 connectDB();
@@ -15,21 +15,17 @@ connectDB();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running....");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
