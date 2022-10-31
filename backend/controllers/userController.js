fa9bad5679181952be02b7e4e1fdd75af90e9dc3
itxSaaad <saadstudent.cs@gmail.com>
2022-10-31 14:05:46 +0500

    Added: User Registeration and Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,33 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register a New User
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -41,4 +68,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
