1f458d8fa09d14f757164bf61380689f47778e17
itxSaaad <saadstudent.cs@gmail.com>
2022-11-16 22:06:47 +0500

    Added: Get User by ID & Update User Enpdpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -113,9 +113,49 @@ const deleteUser = asyncHandler(async (req, res) => {
   if (user) {
     await user.remove();
     res.json({ message: "User Removed" });
-  } else res.status(404);
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
 
-  throw new Error("User Not Found");
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
@@ -125,4 +165,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
